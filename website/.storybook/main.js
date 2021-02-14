caf37123898f4ad16934d3347a53b76bc7c4d3d7
Hossein Rahimi <rhosseinr.h@gmail.com>
2021-02-14 13:21:42 +0330

    add html preview addon

--- website/.storybook/main.js
+++ website/.storybook/main.js
@@ -14,5 +14,6 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-storysource',
     '@storybook/addon-a11y',
+    '@whitespace/storybook-addon-html',
   ],
 };
--- website/.storybook/preview.js
+++ website/.storybook/preview.js
@@ -28,8 +28,8 @@ addDecorator(withA11y);
 // Gatsby's Link overrides:
 // Gatsby defines a global called ___loader to prevent its method calls from creating console errors you override it here
 global.___loader = {
-  enqueue: () => {},
-  hovering: () => {},
+  enqueue: () => { },
+  hovering: () => { },
 };
 
 // Gatsby internal mocking to prevent unnecessary errors in storybook testing environment
@@ -38,4 +38,14 @@ global.__PATH_PREFIX__ = '';
 // This is to utilized to override the window.___navigate method Gatsby defines and uses to report what path a Link would be taking us to if it wasn't inside a storybook
 window.___navigate = (pathname) => {
   action('NavigateTo:')(pathname);
+};
+
+export const parameters = {
+  html: {
+    prettier: {
+      tabWidth: 4,
+      useTabs: false,
+      htmlWhitespaceSensitivity: 'strict',
+    },
+  },
 };
