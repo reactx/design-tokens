c28227b3f688863a2f108af7bd8f518bc6b6256d
behzad888 <behzad88.2012@gmail.com>
2021-03-11 02:14:25 +0330

    remove miscelaneous files

--- website/src/pages/index.jsx
+++ website/src/pages/index.jsx
@@ -7,18 +7,7 @@ import { SEO } from '../components/SEO';
 const IndexPage = () => (
   <DefaultLayout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <p>
-      <Link to="/page-2/">Go to page 2</Link>
-    </p>
-    <p>
-      <Link to="/principles/">Go to the principles page (in MDX)</Link>
-    </p>
-    <p>
-      <Link to="/test/">Load an MDX test page</Link>
-    </p>
+    <h1>Hi people</h1>   
   </DefaultLayout>
 );
 
