91b5672b4f125ab3f85b8bb5bc36bb2c3bd28f2d
Hossein <rhosseinr@outlook.com>
2021-03-17 15:17:35 +0330

    change badge

--- website/src/components/components/Badge.stories.jsx
+++ website/src/components/components/Badge.stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Badge } from './Badge';
+import { Button } from './Button';
 
 export default {
   title: 'Components/Badge',
@@ -60,3 +61,10 @@ export default {
 
 export const Basic = (args) => <Badge {...args} />;
 Basic.args = { children: 'reactx' };
+export const WithButton = (args) => (
+  <Button>
+    <span>Notification</span>
+    <Badge {...args} />
+  </Button>
+);
+WithButton.args = { children: '1', color: 'primary' };
